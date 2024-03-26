410ccbd8b2cd55ded7e939ecb3209aee52de37a0
siva-ee21s125 <105312545+siva-ee21s125@users.noreply.github.com>
2024-03-27 03:24:50 +0530

    Updated test.py with import of NN

--- Assignment-1/test.py
+++ Assignment-1/test.py
@@ -8,6 +8,8 @@ from keras.datasets import fashion_mnist
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from PIL import Image
 
+from FeedForwardNN import neuralNetwork
+
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data();
 
 x_train = x_train.astype('float32') / 255;
